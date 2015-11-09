68ea8e46fa3a6d5515089a579ca18511748b364e
FA12-BSE-170 <FA12-BSE-170@LAB7C-03.cns.ciit>
2015-11-09 15:36:43 +0500

    commit2

--- Egit_demo/src/Egit.java
+++ Egit_demo/src/Egit.java
@@ -4,6 +4,7 @@ public class Egit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("lets do something using GIT...");
+System.out.println("lets not do something :P ");
 	}
 
 }
